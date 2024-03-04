da413ee7615d39c3af54d7d37e3941ac4602d836
Harrison <harrisonchialuka@gmail.com>
2024-03-04 23:18:35 +0100

    project algorithms and sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to bubble sort an array of integers
+ * @array: a pointer to an array of integers
+ * @size: size of the array
+ * Return: nothing is returned
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+project: sorting algorithm for ALX SE programme
+
+contributors: Onuoha Harrison and Slyvanus Uzor
+
+
