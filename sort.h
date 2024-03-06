babe0c35ee985ff384cacee615f8652988041409
Harrison <harrisonchialuka@gmail.com>
2024-03-06 19:50:44 +0100

    sort.h

--- sort.h
+++ sort.h
@@ -34,6 +34,6 @@ void counting_sort(int *array, size_t size);
 void swap_node_ahead(listint_t **head, listint_t **tail, listint_t **swapping);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void cocktail_sort_list(listint_t **head);
-
+void merge_sort(int *array, size_t size);
 
 #endif
